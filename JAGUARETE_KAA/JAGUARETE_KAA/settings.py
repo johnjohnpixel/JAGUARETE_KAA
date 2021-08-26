d5ef593c53043d0d12fa3934db171c2bf53117c2
John <a.jonathan.messina@gmail.com>
2021-08-26 03:11:56 -0300

    Update settings.py

--- JAGUARETE_KAA/JAGUARETE_KAA/settings.py
+++ JAGUARETE_KAA/JAGUARETE_KAA/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4=zg7s*1!3ga(oryv_mq*7cd&+)w!4vnt78n(b*x2c8sz_o@*i'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
